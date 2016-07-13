4df69b3d247a4718305bd64b3d32e92b47fb994f
Francisco Vides <pacovides@gmail.com>
2016-07-12 20:58:52 -0700

    Initial commit

---
+++ src/main/java/com/pacovides/pacofolio/PacofolioApplication.java
@@ -0,0 +1,12 @@
+package com.pacovides.pacofolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacofolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacofolioApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pacovides/pacofolio/PacofolioApplicationTests.java
@@ -0,0 +1,18 @@
+package com.pacovides.pacofolio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PacofolioApplication.class)
+@WebAppConfiguration
+public class PacofolioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
